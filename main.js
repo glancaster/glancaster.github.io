c9a49ec129e72fa437b6c9a3983a43637107841f
glancaster <sailthesouth@gmail.com>
2024-03-06 22:42:36 -0500

    Redoing Website

---
+++ main.js
@@ -0,0 +1,18 @@
+// List of p5.js sketch files you want to choose from
+const sketches = [
+  "sketches/lsystem.js",
+  "sketches/flock.js",
+  "sketches/gameoflife.js",
+  "sketches/bounceballs.js",
+  "sketches/brownian.js",
+  "sketches/particle.js",
+];
+
+// Randomly select a p5.js sketch file
+const randomSketchIndex = Math.floor(Math.random() * sketches.length);
+const randomSketch = sketches[randomSketchIndex];
+
+// Dynamically load the selected p5.js sketch file
+const scriptElement = document.createElement("script");
+scriptElement.src = randomSketch;
+document.getElementById("p5sketch").appendChild(scriptElement);
